3d4168f929e9df6d625d187427eef69e349b5262
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-06-01 13:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const tableParent = document.getElementById('sampleTable');
-const insertBtn = document.querySelector('input[type="button"]'); // Select button by type "button"
+const insertBtn = document.querySelector('input[type="button"]');
 
+insertBtn.addEventListener('click', insert_Row);
 
 function insert_Row() {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-    <td>New Cell1</td>
-    <td>New Cell2</td>
-    `;
-    tableParent.prepend(row);
+    const newRow = tableParent.insertRow(0);
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    cell1.textContent = 'New Cell1';
+    cell2.textContent = 'New Cell2';
 }
